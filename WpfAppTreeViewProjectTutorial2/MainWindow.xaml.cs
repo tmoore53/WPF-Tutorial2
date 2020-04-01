efdfa5a6f2a914407b5a9ec4ac1ffbfef12ebbe6
Tyler Moore <tyler.moore53@outlook.com>
2020-04-01 11:23:50 -0700

    Commit Updates : Binding Drive headers

--- WpfAppTreeViewProjectTutorial2/MainWindow.xaml.cs
+++ WpfAppTreeViewProjectTutorial2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,34 @@ using System.Windows.Shapes;
 
 namespace WpfAppTreeViewProjectTutorial2
 {
+    
+
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach ( var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+
+                FolderView.Items.Add(item);
+                item.Header = drive;
+            }
+
+        }
     }
+    
+
+
 }
