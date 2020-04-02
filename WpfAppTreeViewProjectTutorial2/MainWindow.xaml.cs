0aaa94765a4180a2ef46a5a5148b46382c53a20a
Tyler Moore <tyler.moore53@outlook.com>
2020-04-01 22:06:34 -0700

    Commit Additional code to view inside drives.

--- WpfAppTreeViewProjectTutorial2/MainWindow.xaml.cs
+++ WpfAppTreeViewProjectTutorial2/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 
 namespace WpfAppTreeViewProjectTutorial2
 {
+    ///At the 42:00 minute mark in the video.
     
 
     
@@ -31,19 +32,126 @@ namespace WpfAppTreeViewProjectTutorial2
         }
         #endregion
 
+        #region On Loaded
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+
+            //Get every drive on the machine.
             foreach ( var drive in Directory.GetLogicalDrives())
             {
+                //Create a new item for it.
                 var item = new TreeViewItem();
 
-                FolderView.Items.Add(item);
+                
+                //Set the header and path.
                 item.Header = drive;
+
+                //Set the full path
+                item.Tag = drive;
+
+                //Add a empty item.
+                item.Items.Add(null);
+
+                //Look out for item being expanded.
+                item.Expanded += Folder_Expanded;
+
+                //Add it to the main tree-view.
+                FolderView.Items.Add(item);
             }
 
         }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            //If the item only contains the empty data
+            if (item.Items.Count != 1 && item.Items[0] != null)
+                return;
+
+            //Clear empty item
+            item.Items.Clear();
+
+            //Get the path.
+            var fullpath = (string)item.Tag;
+
+            //Create a empty list.
+            var directories = new List<string>();
+
+
+             //Try and get directories from the folder
+             //Ignoring any errors.
+            try
+            {
+                var dirs = Directory.GetDirectories(fullpath);
+
+                if (dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+
+                }
+            }
+            catch (Exception )
+            { }
+
+            //
+            directories.ForEach(directoryPath =>
+            {
+                //Create a directory item. 
+                var subItem = new TreeViewItem()
+                {
+                    //Set the header and tag.
+                    Header = GetFileFolderName(directoryPath),
+                    Tag = directoryPath
+
+                };
+                
+                //Add a empty item to enable expanding folder.
+                subItem.Items.Add(null);
+
+                //Handle expanding recursion.
+                subItem.Expanded += Folder_Expanded;
+
+
+                //Add this subItem to the parent Item
+                item.Items.Add(subItem);
+
+            });
+
+
+        }
+
+        #endregion
+        /// <summary>
+        /// Find the folder name from path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string GetFileFolderName(string path)
+        {
+            //C: \ Something Folder \ a folder
+            if (string.IsNullOrEmpty(path))
+            return string.Empty;
+
+            //Replace all the forward slashes into back slashes
+            // puting two backslashes in so that it doesn't think it is a special
+            //character to tab out or net line (\n, \t)
+            var normalizedPath = path.Replace('/', '\\');
+
+            //Find the last 
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+
+            //If there is no backslash return the path itself
+            if (lastIndex <=0)
+            return path;
+            
+            //Return the name after the backslash
+            return path.Substring(lastIndex + 1);
+
+        }
     }
-    
+
 
 
 }
