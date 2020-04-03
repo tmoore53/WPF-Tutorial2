36c7a7ea66432ab797eddf9be4cc77848dc9d3b7
Tyler Moore <tyler.moore53@outlook.com>
2020-04-03 11:09:22 -0700

    Commit final project with two different images

--- WpfAppTreeViewProjectTutorial2/HeaderToImageConverter.cs
+++ WpfAppTreeViewProjectTutorial2/HeaderToImageConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,28 @@ namespace WpfAppTreeViewProjectTutorial2
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            //Find the full path
             var path = (string)value;
+
+            //Make sure the path is not null
             if (path == null)
                 return null;
+            
+
+            //Getting the name of the file or folder
+            var name = MainWindow.GetFileFolderName(path);
+
+            var image = "Images/Drive-icon_image.png";
+
+            if (string.IsNullOrEmpty(name))
+                image = "Images/Drive-icon_image.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/Closed _Folder_image.png";
+
             //This accesses resources in WPF as URI's use the string = ($"pack://application:,,,/{Whatever resourse you want}") ) 
-            return new BitmapImage(new Uri($"pack://application:,,,/Images/Drive-icon_image.png"));//Fix this
+            //return new BitmapImage(new Uri($"pack://application:,,,/Images/Drive-icon_image.png"));
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
